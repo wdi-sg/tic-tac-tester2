ecabdaf8a0c655d5a76cfb6e8f53858b078aba2d
Daniel Lim <ddanielnp@hotmail.com>
2017-06-28 18:01:42 +0800

    first commit

---
+++ testing-the-tester.js
@@ -0,0 +1,37 @@
+function restart () {
+
+}
+function isGameOver () {
+  return false
+}
+function whoWon () {
+  return 0
+}
+
+function playTurn (index) {
+  if (isGameOver()) {
+    alert('game over')
+  } else {
+
+  }
+}
+
+var grid = [0,0,0,0,0,0,0,0,0,0]
+
+var playerOne = [0,0,0,0,0,0,0,0,0]
+var playerTwo = [0,0,0,0,0,0,0,0,0]
+
+var winArr = [
+  [9, 9, 9, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 9, 9, 9, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 9, 9, 9],
+  [9, 0, 0, 9, 0, 0, 9, 0, 0],
+  [0, 9, 0, 0, 9, 0, 0, 9, 0],
+  [0, 0, 9, 0, 0, 9, 0, 0, 9],
+  [9, 0, 0, 0, 9, 0, 0, 0, 9],
+  [0, 0, 9, 0, 9, 0, 9, 0, 0]
+]
+
+function checkGrid(arr) {
+  if ()
+}
