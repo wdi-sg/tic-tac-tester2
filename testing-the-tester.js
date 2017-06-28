4750f365824fced37a8f070b83b5d144541b78b3
lee brian <leesinshingbrian@gmail.com>
2017-06-28 22:55:30 +0800

    tried again

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,5 +1,5 @@
 var arr = [true, true, true, true, true, true, true, true, true ]
-var counter = 1
+var counter = 0
 var winner = 0
 var winningMoves = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [3, 4, 5], [6, 7, 8], [2, 4, 6]]
 var player1 = []
