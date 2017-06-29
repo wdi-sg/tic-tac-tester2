553686f33b9ca86063a325b0f584c19e480c3f9d
Daniel Lim <ddanielnp@hotmail.com>
2017-06-29 16:56:49 +0800

    update DOM HTML CSS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// DOM MANIPULATION JS CODES
 document.addEventListener('DOMContentLoaded', init)
 
+var ttt = ttt()
 function init () {
   var b0 = document.querySelector('.b0')
   var b1 = document.querySelector('.b1')
@@ -20,20 +22,47 @@ function init () {
   b6.addEventListener('click', playSquare)
   b7.addEventListener('click', playSquare)
   b8.addEventListener('click', playSquare)
+
+  var button = document.querySelector('.button')
+  button.addEventListener('click', resetButton)
 }
 
 function playSquare (event) {
   var elem = event.target.classList
-
+  var moveAllowed = false
   for (i = 0; i < 9; i++) {
     if (elem.contains('b' + i)) {
-      console.log(playTurn(i))
+      if (ttt.playTurn(i)) {
+        // player1 starts as blue(X)
+        if (playerTurn % 2 == 0) {
+          elem.add('X')
+        } else elem.add('O')
+      }
     }
+  }
+  if (ttt.whoWon() == 1) {
+    setTimeout(function () {
+      alert('Player 1 wins! Game will restart now.')
+    }, 200)
+  } else if (ttt.whoWon() == 2) {
+    setTimeout(function () {
+      alert('Player 2 wins! Game will restart now.')
+    }, 200)
+  } else if (ttt.whoWon() == 3) {
+    setTimeout(function () {
+      alert('Game Tied Game will restart now.')
+    }, 200)
+  } if (ttt.isGameOver()) {
+    ttt.restart()
+    setTimeout(function () {
+      window.location.reload(true)
+    }, 6000)
   }
 }
 
-function fillSquare () {
-
+function resetButton () {
+  ttt.restart()
+  setTimeout(function () {
+    window.location.reload(true)
+  })
 }
-
-  // console.log(elem)
--- testing-the-tester.js
+++ testing-the-tester.js
@@ -17,89 +17,91 @@ var winArr = [
 ]
 
 // pass one test at a time
-function restart () {
-  grid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  playerOne1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  playerTwo1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  turnCounter = 0
-  playerTurn = 1
-}
+function ttt () {
 
-function ArrEqual (arr1, arr2) {
-  var isEqual = true
-  if (arr1.length === arr2.length) {
-    for (var i = 0; i < arr1.length; i++) {
-      if (arr1[i] != arr2[i]) {
-        isEqual = false
-        break
+  function arrEqual (arr1, arr2) {
+    var isEqual = true
+    if (arr1.length === arr2.length) {
+      for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] != arr2[i]) {
+          isEqual = false
+          break
+        }
       }
+    } else {
+      isEqual = false
     }
-  } else {
-    isEqual = false
+    return isEqual
   }
-  return isEqual
-}
 
-function isGameOver () {
-  if (whoWon() === 1 || whoWon() === 2 || whoWon() === 3) {
-    return true
+  function restart () {
+    grid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    playerOne1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    playerTwo1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    turnCounter = 0
+    playerTurn = 1
   }
-  return false
-}
 
-function whoWon () {
-  var gameStatus = 0
-  var noWinnerYet = true
-  var TestWinScenarios = 0
-  if (turnCounter === 9) {
-    gameStatus = 3
-  } else {
-    while (noWinnerYet && TestWinScenarios < 8) {
-      // console.log('p1 '+TestWinScenarios)
-      if (ArrEqual(playerOne1, winArr[TestWinScenarios])) {
-        gameStatus = 1
-        noWinnerYet = false
-      }
-      TestWinScenarios += 1
+  function isGameOver () {
+    if (whoWon() === 1 || whoWon() === 2 || whoWon() === 3) {
+      return true
     }
-    if (noWinnerYet) {
-      TestWinScenarios = 0
+    return false
+  }
+
+  function whoWon () {
+    var gameStatus = 0
+    var noWinnerYet = true
+    var TestWinScenarios = 0
+    if (turnCounter === 9) {
+      gameStatus = 3
+    } else {
       while (noWinnerYet && TestWinScenarios < 8) {
-        // console.log('p2 '+TestWinScenarios)
-        if (ArrEqual(playerTwo1, winArr[TestWinScenarios])) {
-          gameStatus = 2
+    // console.log('p1 '+TestWinScenarios)
+        if (arrEqual(playerOne1, winArr[TestWinScenarios])) {
+          gameStatus = 1
           noWinnerYet = false
         }
         TestWinScenarios += 1
       }
+      if (noWinnerYet) {
+        TestWinScenarios = 0
+        while (noWinnerYet && TestWinScenarios < 8) {
+      // console.log('p2 '+TestWinScenarios)
+          if (arrEqual(playerTwo1, winArr[TestWinScenarios])) {
+            gameStatus = 2
+            noWinnerYet = false
+          }
+          TestWinScenarios += 1
+        }
+      }
     }
-
-    //  for (var i = 0; i < winArr.length; i++) {
-    //    if (ArrEqual(playerOne1, winArr[i])) {
-    //      GameStatus = 1
-    //      break
-    //    } else if (ArrEqual(playerTwo1, winArr[i])) {
-    //      GameStatus = 2
-    //      break
-    //  }
+    return gameStatus
   }
-  return gameStatus
-}
 
-function playTurn (index) {
-  var isAllowed = true
-  if (grid[index] === 0) {
-    grid[index] = 9
-    turnCounter += 1
-    if (playerTurn === 1) {
-      playerOne1[index] = 9
-      playerTurn = 2
+  function playTurn (index) {
+    var isAllowed = true
+    if (grid[index] === 0) {
+      grid[index] = 9
+      turnCounter += 1
+      if (playerTurn === 1) {
+        playerOne1[index] = 9
+        playerTurn = 2
+      } else {
+        playerTwo1[index] = 9
+        playerTurn = 1
+      }
     } else {
-      playerTwo1[index] = 9
-      playerTurn = 1
+      isAllowed = false
     }
-  } else {
-    isAllowed = false
+    return isAllowed
   }
-  return isAllowed
+
+  return {
+    arrEqual: arrEqual,
+    restart: restart,
+    isGameOver: isGameOver,
+    whoWon: whoWon,
+    playTurn: playTurn
+  }
 }
