1404cebf338b82a42c02d5d9b1e29112dd1fd375
Daniel Lim <ddanielnp@hotmail.com>
2017-06-29 09:16:55 +0800

    update final

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,25 +1,9 @@
-function restart () {
+var grid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-}
-function isGameOver () {
-  return false
-}
-function whoWon () {
-  return 0
-}
-
-function playTurn (index) {
-  if (isGameOver()) {
-    alert('game over')
-  } else {
-
-  }
-}
-
-var grid = [0,0,0,0,0,0,0,0,0,0]
-
-var playerOne = [0,0,0,0,0,0,0,0,0]
-var playerTwo = [0,0,0,0,0,0,0,0,0]
+var playerOne1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var playerTwo1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+var turnCounter = 0
+var playerTurn = 1
 
 var winArr = [
   [9, 9, 9, 0, 0, 0, 0, 0, 0],
@@ -32,6 +16,90 @@ var winArr = [
   [0, 0, 9, 0, 9, 0, 9, 0, 0]
 ]
 
-function checkGrid(arr) {
-  if ()
+// pass one test at a time
+function restart () {
+  grid = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  playerOne1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  playerTwo1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  turnCounter = 0
+  playerTurn = 1
+}
+
+function ArrEqual (arr1, arr2) {
+  var isEqual = true
+  if (arr1.length === arr2.length) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        isEqual = false
+        break
+      }
+    }
+  } else {
+    isEqual = false
+  }
+  return isEqual
+}
+
+function isGameOver () {
+  if (whoWon() === 1 || whoWon() === 2 || whoWon() === 3) {
+    return true
+  }
+  return false
+}
+
+function whoWon () {
+  var gameStatus = 0
+  var noWinnerYet = true
+  var TestWinScenarios = 0
+  if (turnCounter === 9) {
+    gameStatus = 3
+  } else {
+    while (noWinnerYet && TestWinScenarios < 8) {
+      // console.log('p1 '+TestWinScenarios)
+      if (ArrEqual(playerOne1, winArr[TestWinScenarios])) {
+        gameStatus = 1
+        noWinnerYet = false
+      }
+      TestWinScenarios += 1
+    }
+    if (noWinnerYet) {
+      TestWinScenarios = 0
+      while (noWinnerYet && TestWinScenarios < 8) {
+        // console.log('p2 '+TestWinScenarios)
+        if (ArrEqual(playerTwo1, winArr[TestWinScenarios])) {
+          gameStatus = 2
+          noWinnerYet = false
+        }
+        TestWinScenarios += 1
+      }
+    }
+
+    //  for (var i = 0; i < winArr.length; i++) {
+    //    if (ArrEqual(playerOne1, winArr[i])) {
+    //      GameStatus = 1
+    //      break
+    //    } else if (ArrEqual(playerTwo1, winArr[i])) {
+    //      GameStatus = 2
+    //      break
+    //  }
+  }
+  return gameStatus
+}
+
+function playTurn (Index) {
+  var isAllowed = true
+  if (grid[Index] === 0) {
+    grid[Index] = 9
+    turnCounter += 1
+    if (playerTurn === 1) {
+      playerOne1[Index] = 9
+      playerTurn = 2
+    } else {
+      playerTwo1[Index] = 9
+      playerTurn = 1
+    }
+  } else {
+    isAllowed = false
+  }
+  return isAllowed
 }
