c60885c03954581074c566f10e5bc9f3e2d9748a
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-29 12:27:29 +0800

    added board, event listeners

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,3 +1,4 @@
+
 var grid = [] // what has been played so far
 
 function restart () {
@@ -14,30 +15,30 @@ function whoWon () {
   var player2Moves = []
   var winningCombi = ['012', '345', '678', '036', '147', '258', '246', '048']
 
-  //split into two player arrays
+  // split into two player arrays
   for (var i = 0; i < grid.length; i++) {
     if (i % 2 === 0) {
       player1Moves.push(grid[i])
     } else player2Moves.push(grid[i])
   }
 
-  //sort player's moves numerically from smallest to largest
-  player1sorted = player1Moves.sort(function(a,b) {
+  // sort player's moves numerically from smallest to largest
+  player1sorted = player1Moves.sort(function (a, b) {
     return a - b
   })
-  player2sorted = player2Moves.sort(function(a,b) {
+  player2sorted = player2Moves.sort(function (a, b) {
     return a - b
   })
 
-  //make arr a string and check if present in winningCombi
-  if (winningCombi.includes(player1sorted.join(""))) { // need to sort player moves from 0-8
+  // make arr a string and check if present in winningCombi
+  if (winningCombi.includes(player1sorted.join(''))) { // need to sort player moves from 0-8
     winner = 1
   }
-  if (winningCombi.includes(player2sorted.join(""))) { // need to sort player moves from 0-8
+  if (winningCombi.includes(player2sorted.join(''))) { // need to sort player moves from 0-8
     winner = 2
   }
 
-  //check if draw
+  // check if draw
   if (grid.length === 9) {
     winner = 3
   }
@@ -48,10 +49,23 @@ function whoWon () {
 // it would have returned earlier
 
 function playTurn (index) {
-  if (grid.includes(index)) {
+  if (grid.includes(index) || isGameOver()) {
     return false
   } else {
     grid.push(index)
     return true
   }
 }
+
+var counter = 0;
+
+var allDivs = document.querySelectorAll('div')
+allDivs.forEach(function(el) {
+  el.addEventListener('click', function() {
+    if (counter % 2) {
+        this.innerText = 'O'
+    } else this.innerText = 'X'
+    counter ++
+  console.log(this.className)
+  })
+})
