4f9531a7a366cb13370b8408bee97f102a65ced2
Shaun Loh <loh.shaun89@gmail.com>
2017-06-29 09:14:10 +0800

    created game functions

---
+++ testing-the-tester.js
@@ -0,0 +1,120 @@
+var chosen = []
+
+var grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+var player1 = []
+
+var player2 = []
+
+// winning combinations by row
+var winning1 = [0, 1, 2]
+var winning2 = [3, 4, 5]
+var winning3 = [6, 7, 8]
+
+// winning combinations by column
+var winning4 = [0, 3, 6]
+var winning5 = [1, 4, 7]
+var winning6 = [2, 5, 8]
+
+// winning combinations by diagonal
+var winning7 = [0, 4, 8]
+var winning8 = [2, 4, 6]
+
+function restart () {
+  chosen = []
+  player1 = [] // clears the player1 array
+  player2 = [] // clears the player2 array
+}
+
+function isGameOver () {
+  return false
+}
+
+function whoWon () {
+// if game not finished, return 0
+// if player1 wins, return 1
+// if player2 wins, return 2
+// if draw, return 3
+var winner = 0 // by default, whoWon returns 0 at start/during game
+
+  for (var i = 0; i < chosen.length; i++) {
+    if (i % 2 === 0) {
+      player1.push(chosen[i])
+    } else {
+      player2.push(chosen[i])
+    }
+  }
+
+// rearrange player 1 arr smallest to highest
+  var player1Sorted = player1.sort(function (a, b) {
+    return a - b
+  })
+  var player2Sorted = player2.sort(function (a, b) {
+    return a - b
+  })
+
+// check if player1 wins
+  if (player1Sorted.toString() === winning1.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning2.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning3.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning4.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning5.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning6.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning7.toString()) {
+    winner = 1
+  }
+  if (player1Sorted.toString() === winning8.toString()) {
+    winner = 1
+  }
+  if (player2Sorted.toString() === winning1.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning2.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning3.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning4.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning5.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning6.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning7.toString()) {
+    winner = 2
+  }
+  if (player2Sorted.toString() === winning8.toString()) {
+    winner = 2
+  } else if (chosen.length === 9) {
+    return 3
+  }
+  return winner
+}
+
+function playTurn (index) {
+// index is what player choses
+// if index is in chosen, return false
+  for (var i = 0; i < chosen.length; i++) {
+    if (index === chosen[i]) {
+      return false
+    }
+  }
+  chosen.push(index)
+  return true
+}
