76d274cc9a04371382e2dabd34b303887f15abbc
Nur Hidayah <dayah@Seths-MacBook.local>
2017-09-24 17:13:20 +0800

    playTurn, restart stable. isGameOver not stable?

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-
-
-
-function restart() {
-  isGameOver();
-
-}
-
-//logic that checks if game is over or not
-//the game is over only wheennnn
-//the game is NOT over when the grid array values are all '0'
 var total = 0;
-
-var total = 0;
-
-function isGameOver () {
-  for (i = 0; i < grid.length; i ++) {
-        total += grid[i]
-  }
-        console.log ('total' + " " + total)
-        if (total === 0) {
-            console.log (false)
-      }
-    else
-     {
-       console.log(true)
-     }
-
-}
-
-function whoWon () {
- if (isGameOver == true) {
-   return 1
- }
- else {
-   return 0
- }
-}
-
-//playTurn
-//check if it is ataken up or not
-//so if the
+var numOfMoves = 0;
+var moves = []
 var grid = [0,0,0,
 0,0,0,
 0,0,0]
 
+
+function restart() {
+  for (i = 0; i < grid.length; i ++) {
+        total += grid[i]
+        continue
+  }
+  isGameOver()
+  whoWon()
+}
+
+function isGameOver (){
+  if (total === 0 ) {
+          return false
+          }
+          else {
+            return 0
+          }
+    }
+// }
+//       // console.log ('total' + " " + total)
+
+//logic that checks if game is over or not
+//the game is over only wheennnn
+//the game is NOT over when the grid array values are all '0'
+
+
+//game DEFINITELY not finished if the grid.length less than 5
+//this one return 0
+//if winner = playerOne, return 1
+//if winner = playerTwo, return 2
+//if winner = playerDraw, return 3
+
+
+//playTurn
+//check if it is ataken up or not
+//so if the
 
 
 function playTurn(num){
@@ -57,3 +54,26 @@ function playTurn(num){
     return false
   }
 }
+
+
+function whoWon () {
+  switch (moves.length) {
+  case (5) :
+    winner = playerOne
+    console.log (playerOne)
+    break;
+  case (6) :
+    winner = playerTwo
+    break;
+  case (9) :
+    winner = playerDraw
+    break;
+  default:
+  return 0
+}
+}
+
+
+
+  //if the moves array length is 5, winner is playerOne
+  //if the moves array length is 6, winner is playerTwo
