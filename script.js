db65caf24060fceecec117ada6a178cd2fc7b921
Nur Hidayah <dayah@Seths-MacBook.local>
2017-09-24 18:01:30 +0800

    whoWon still not perfect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var total = 0;
-var numOfMoves = 0;
-var moves = []
+var total = 0
+// var moves = []
+// var totalMoves = moves.length
 var grid = [0,0,0,
 0,0,0,
 0,0,0]
@@ -8,39 +8,22 @@ var grid = [0,0,0,
 
 function restart() {
   for (i = 0; i < grid.length; i ++) {
+    //for every run of the grid, replace the grid[i] with 0
+    grid[i] = 0
         total += grid[i]
-        continue
-  }
-  isGameOver()
-  whoWon()
+        isGameOver ()
+        }
 }
 
-function isGameOver (){
-  if (total === 0 ) {
-          return false
-          }
-          else {
-            return 0
-          }
+function isGameOver () {
+  if (whoWon == true) {
+    return true
+  }
+  else {
+    return false
+  }
     }
-// }
-//       // console.log ('total' + " " + total)
 
-//logic that checks if game is over or not
-//the game is over only wheennnn
-//the game is NOT over when the grid array values are all '0'
-
-
-//game DEFINITELY not finished if the grid.length less than 5
-//this one return 0
-//if winner = playerOne, return 1
-//if winner = playerTwo, return 2
-//if winner = playerDraw, return 3
-
-
-//playTurn
-//check if it is ataken up or not
-//so if the
 
 
 function playTurn(num){
@@ -56,24 +39,36 @@ function playTurn(num){
 }
 
 
-function whoWon () {
-  switch (moves.length) {
-  case (5) :
-    winner = playerOne
-    console.log (playerOne)
-    break;
-  case (6) :
-    winner = playerTwo
-    break;
-  case (9) :
-    winner = playerDraw
-    break;
-  default:
+function whoWon (winner) {
+  if (winner == false) {
   return 0
 }
+else {
+  return winner
 }
-
-
-
+}
   //if the moves array length is 5, winner is playerOne
   //if the moves array length is 6, winner is playerTwo
+
+
+  // }
+  //       // console.log ('total' + " " + total)
+
+  //logic that checks if game is over or not
+  //the game is over only wheennnn
+  //the game is NOT over when the grid array values are all '0'
+
+
+  //game DEFINITELY not finished if the grid.length less than 5
+  //this one return 0
+  //if winner = playerOne, return 1
+  //if winner = playerTwo, return 2
+  //if winner = playerDraw, return 3
+
+
+  //playTurn
+  //check if it is ataken up or not
+  //so if the
+
+
+  //moves.length is not defined because it's not global. It works within the loop but not when restart game
