cdb94bb40d7f7337be7cba41dfa91a085b59f465
sm <a0086845@u.nus.edu>
2017-09-25 09:36:00 +0800

    removed brackets

--- script.js
+++ script.js
@@ -11,51 +11,30 @@ function restart() {
 
 function isGameOver() {
   // console.log("Game Over")
-  if (whoWon() === 0) {
-    return false
-  } else {
-    return true
-  }
+  if (whoWon() === 0) return false
+  else return true
 }
 
 function whoWon() {
-  if (turns === 0) {
-    return 0
-  } else {
-    if (grid[0] === grid[1] && grid[1] === grid[2]) {
-      return grid[0]
-    } else if (grid[3] === grid[4] && grid[4] === grid[5]) {
-      return grid[3]
-    } else if (grid[6] === grid[7] && grid[7] === grid[8]) {
-      return grid[6]
-    } else if (grid[0] === grid[3] && grid[3] === grid[6]) {
-      return grid[0]
-    } else if (grid[1] === grid[4] && grid[4] === grid[7]) {
-      return grid[1]
-    } else if (grid[2] === grid[5] && grid[5] === grid[8]) {
-      return grid[2]
-    } else if (grid[0] === grid[4] && grid[4] === grid[8]) {
-      return grid[0]
-    } else if (grid[2] === grid[4] && grid[4] === grid[6]) {
-      return grid[2]
-    } else if (turns === 9) {
-      return 3
-    } else {
-      console.log(grid)
-      return 0
-    }
-  }
+  if (turns === 0) return 0
+  else if (grid[0] === grid[1] && grid[1] === grid[2]) return grid[0]
+  else if (grid[3] === grid[4] && grid[4] === grid[5]) return grid[3]
+  else if (grid[6] === grid[7] && grid[7] === grid[8]) return grid[6]
+  else if (grid[0] === grid[3] && grid[3] === grid[6]) return grid[0]
+  else if (grid[1] === grid[4] && grid[4] === grid[7]) return grid[1]
+  else if (grid[2] === grid[5] && grid[5] === grid[8]) return grid[2]
+  else if (grid[0] === grid[4] && grid[4] === grid[8]) return grid[0]
+  else if (grid[2] === grid[4] && grid[4] === grid[6]) return grid[2]
+  else if (turns === 9) return 3
+  else return 0
 }
 
 function playTurn(moves) {
   if (grid[moves] === 0) {
     grid[moves] = currentPlayer
     turns++
-    if (currentPlayer === 1) {
-      currentPlayer = 2
-    } else currentPlayer = 1
+    if (currentPlayer === 1) currentPlayer = 2
+    else currentPlayer = 1
     return true
-  } else {
-    return false
-  }
+  } else return false
 }
